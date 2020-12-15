10dbd4927f87cb735260cf1775f47b7600778050
Gustavo Felipe Batista Carneiro <gustavo.felipebc@gmail.com>
2020-12-15 08:01:41 -0300

    implementaçao de scroll suave nas trasiçoes ao clicar nos links internos

---
+++ script.js
@@ -0,0 +1,16 @@
+const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+    
+    section.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+}
+
+linksInternos.forEach((link) => {
+    link.addEventListener('click', scrollToSection);
+})
