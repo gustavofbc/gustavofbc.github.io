6d41034ff1bb628fc2e99dd5ae0c010e7bb0a445
gustavofbc <gustavo.felipebc@gmail.com>
2021-12-17 17:44:12 -0300

    implemented menu accessibility and improvements in your css

--- navbar.js
+++ navbar.js
@@ -1,8 +1,21 @@
 const btnMenu = document.getElementById('btn-menu');
 
-function toggleMenu(params) {
+function toggleMenu(event) {
+  if(event.type === 'touchstart') {
+    event.preventDefault();
+  }
+
   const nav = document.getElementById('nav');
   nav.classList.toggle('ativo');
+
+  //! ACESSIIBILIDADE
+  const menuAtivado = nav.classList.contains('ativo');
+  if(menuAtivado){
+    event.currentTarget.setAttribute('aria-label', 'Fechar menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir menu');
+  }
 }
 
 btnMenu.addEventListener('click', toggleMenu);
+btnMenu.addEventListener('touchstart', toggleMenu);
