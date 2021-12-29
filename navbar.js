60dd9a111c8541b44aa111b44c9546fb64c77275
gustavofbc <gustavo.felipebc@gmail.com>
2021-12-29 19:23:45 -0300

    scroll suave implemented in menu

--- navbar.js
+++ navbar.js
@@ -19,3 +19,29 @@ function toggleMenu(event) {
 
 btnMenu.addEventListener('click', toggleMenu);
 btnMenu.addEventListener('touchstart', toggleMenu);
+
+const menuItens = document.querySelectorAll('#menu a[href^="#"]');
+
+
+menuItens.forEach(item => {
+  item.addEventListener('click', scrollToIdOnClick);
+})
+
+function getScrollTopByHref(element) {
+  const id = element.getAttribute('href');
+  return document.querySelector(id).offsetTop;
+}
+
+function scrollToIdOnClick(event) {
+  event.preventDefault();
+  const toSection = getScrollTopByHref(event.currentTarget) - 50;
+
+  scrollToPosition(toSection);
+}
+
+function scrollToPosition(toSection) {
+  window.scroll({
+    top: toSection,
+    behavior: "smooth"
+  });
+}
