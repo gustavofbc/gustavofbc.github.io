b4a54a6cdb3b0f86f79d7cf6563336357f1127b8
gustavo <gustavo.felipebc@gmail.com>
2022-08-26 12:26:40 -0300

    new version! :tada:

--- navbar.js
+++ navbar.js
@@ -1,32 +1,31 @@
-const btnMenu = document.getElementById('btn-menu');
-const nav = document.getElementById('nav');
+const btnMenu = document.getElementById("btn-menu");
+const nav = document.getElementById("nav");
 
 function toggleMenu(event) {
-  if(event.type === 'touchstart') {
+  if (event.type === "touchstart") {
     event.preventDefault();
   }
 
-  nav.classList.toggle('ativo');
+  nav.classList.toggle("ativo");
 
   //! ACESSIIBILIDADE
-  const menuAtivado = nav.classList.contains('ativo');
-  if(menuAtivado){
-    event.currentTarget.setAttribute('aria-label', 'Fechar menu');
+  const menuAtivado = nav.classList.contains("ativo");
+  if (menuAtivado) {
+    event.currentTarget.setAttribute("aria-label", "Fechar menu");
   } else {
-    event.currentTarget.setAttribute('aria-label', 'Abrir menu');
+    event.currentTarget.setAttribute("aria-label", "Abrir menu");
   }
 }
 
-btnMenu.addEventListener('click', toggleMenu);
-btnMenu.addEventListener('touchstart', toggleMenu);
+btnMenu.addEventListener("click", toggleMenu);
+btnMenu.addEventListener("touchstart", toggleMenu);
 
-
-function initScrollSuave(){
+function initScrollSuave() {
   const menuItens = document.querySelectorAll('#menu a[href^="#"]');
-  
-  menuItens.forEach(item => {
-    item.addEventListener('click', scrollToIdOnClick);
-  })
+
+  menuItens.forEach((item) => {
+    item.addEventListener("click", scrollToIdOnClick);
+  });
 
   function scrollToIdOnClick(event) {
     event.preventDefault();
@@ -34,43 +33,41 @@ function initScrollSuave(){
 
     scrollToPosition(toSection);
     //fechar o menu quando um item for selecionado
-    nav.classList.remove('ativo');
+    nav.classList.remove("ativo");
   }
 
   function getScrollTopByHref(element) {
-    const id = element.getAttribute('href');
+    const id = element.getAttribute("href");
     return document.querySelector(id).offsetTop;
   }
 
   function scrollToPosition(toSection) {
     window.scroll({
       top: toSection,
-      behavior: "smooth"
+      behavior: "smooth",
     });
   }
-
 }
 
 initScrollSuave();
 
 function initAnimationToScroll() {
-  const elementosAnimados = document.querySelectorAll('[data-anime]');
-  console.log(elementosAnimados);
+  const elementosAnimados = document.querySelectorAll("[data-anime]");
 
   const windowSeventyPercent = window.innerHeight * 0.7;
 
   function animeToScroll() {
-    elementosAnimados.forEach(section => {
+    elementosAnimados.forEach((section) => {
       const sectionHeightTop = section.getBoundingClientRect().top;
 
-      const isElementoVisible = (sectionHeightTop - windowSeventyPercent) < 0;
-      if(isElementoVisible){
-        section.classList.add('animate');
+      const isElementoVisible = sectionHeightTop - windowSeventyPercent < 0;
+      if (isElementoVisible) {
+        section.classList.add("animate");
       }
-    })
+    });
   }
 
-  window.addEventListener('scroll', animeToScroll);
+  window.addEventListener("scroll", animeToScroll);
 }
 
 initAnimationToScroll();
