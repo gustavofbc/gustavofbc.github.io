ddee4ee01835d51e1a1e18fd37229e46c7cbb7f1
Gustavo Felipe <gustavo.felipebc@gmail.com>
2023-11-13 16:45:08 -0300

    chore: animation in the website presentation logo

---
+++ animationBody.js
@@ -0,0 +1,23 @@
+function displayLogo(){
+  const logoContainer = document.getElementById('logo-container');
+  const myLogo = document.getElementById('my-logo');
+  const header = document.querySelector('header');
+  const main = document.querySelector('main');
+  const footer = document.querySelector('footer');
+  header.style.display = 'none';
+  main.style.display = 'none';
+  footer.style.display = 'none';
+
+  setTimeout(() => {
+    myLogo.style.opacity = '0';
+  }, 3000);
+
+  setTimeout(() => {
+      logoContainer.style.display = 'none';
+      header.style.display = 'flex';
+      main.style.display = 'block';
+      footer.style.display = 'block';
+  }, 4500);
+}
+
+window.onload = displayLogo;
